fb8a914ff4815ee14df2592c77f80e925059fa69
Richard Aubrey White <hello@rwhite.no>
2026-06-30 13:26:54 +0300

    Use unicode escapes for Nordic characters in config object

--- R/config_aa_environments.r
+++ R/config_aa_environments.r
@@ -8,12 +8,12 @@
 #' csdata::nb$AA  # uppercase Aa
 #' @export nb
 nb <- list()
-nb$AA <- "Å"
-nb$aa <- "å"
-nb$OE <- "Ø"
-nb$oe <- "ø"
-nb$AE <- "Æ"
-nb$ae <- "æ"
+nb$AA <- "\u00C5"
+nb$aa <- "\u00E5"
+nb$OE <- "\u00D8"
+nb$oe <- "\u00F8"
+nb$AE <- "\u00C6"
+nb$ae <- "\u00E6"
 
 #' Swedish characters in unicode
 #'
@@ -25,10 +25,10 @@ nb$ae <- "æ"
 #' csdata::se$oe  # lowercase oe
 #' @export se
 se <- list()
-se$OE <- "Ö"
-se$oe <- "ö"
-se$AE <- "Ä"
-se$ae <- "ä"
+se$OE <- "\u00D6"
+se$oe <- "\u00F6"
+se$AE <- "\u00C4"
+se$ae <- "\u00E4"
 
 #' Package configuration environment
 #'
